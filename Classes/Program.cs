01eae365ec6afcbd2a94e5b5afa0b719ff03d96f
RichardRodi <RichardmRodi@gmail.com>
2024-02-26 15:48:26 -0500

    Created a new Class called Car and added properties

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Classes
+﻿using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "Audi";
+            myCar.Model = "A4";
+            myCar.Year = 2021;
+
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
         }
     }
 }
